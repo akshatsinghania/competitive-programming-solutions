96ff6d2d617d938c1dccb0d8f14000144814a28a
akshatsinghania <singhaniaakshat1@gmail.com>
2021-03-12 23:01:50 +0530

    Fixed 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -8,12 +8,12 @@ using namespace std;
 int moves;
 
 int main() {
-    for(int  r = 0;r < 5;++r){
-        for(int c = 0;c < 5; ++c){
+    for(int  r = 1;r <= 5;++r){
+        for(int c = 1;c <= 5; ++c){
             int input;
             cin >> input;
             if(input == 1){
-                moves = abs(r - 3) + abs(c - 3);
+                moves = abs(3 - r) + abs(3 - c);
             }
         }
     }
