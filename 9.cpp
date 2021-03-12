f95ea9c2514b46cd49382a8b27116155265c9145
akshatsinghania <singhaniaakshat1@gmail.com>
2021-03-12 17:04:35 +0530

    Added 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/50/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int M,N;
+
+int main() {
+    cin >> M >> N;
+    cout << (M * N) / 2;
+    return 0;
+}
